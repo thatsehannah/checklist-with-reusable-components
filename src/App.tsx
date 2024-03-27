3130c27328d53617c36a81ff1e49d76f60409077
E. Hannah <echannah631@gmail.com>
2024-03-27 10:00:15 -0500

    created a generic checklist using keyof operator, rendered a basic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Checklist } from './Checklist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-10'>
+      <Checklist
+        data={[
+          { id: 1, name: 'Lucy', role: 'Manager' },
+          { id: 2, name: 'Bob', role: 'Developer' },
+        ]}
+        id='id'
+        primary='name'
+        secondary='role'
+      />
     </div>
   );
 }
